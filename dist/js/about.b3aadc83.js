80dbe83f1b6913e09bb081b036a83d4ecc8738cb
vehymy <selimhanbaltas@gmail.com>
2023-04-01 15:37:47 +0300

    disting

---
+++ dist/js/about.b3aadc83.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_practice_1"]=self["webpackChunkvue_practice_1"]||[]).push([[443],{7381:function(e,n,u){u.r(n),u.d(n,{default:function(){return l}});var t=u(3396);const a={class:"about"},c=(0,t._)("h1",null,"This is an about page",-1),r=[c];function s(e,n){return(0,t.wg)(),(0,t.iD)("div",a,r)}var i=u(89);const o={},f=(0,i.Z)(o,[["render",s]]);var l=f}}]);
+//# sourceMappingURL=about.b3aadc83.js.map
